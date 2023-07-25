d377efa2292329ecd7857a1c2c03543dda4dd2d5
DSLuchenko <DSLuchenko@bk.ru>
2023-07-26 00:32:08 +0300

    fix: change data type exchange_rate.rate NUMERIC(10,4)  and currency.sign VARCHAR(1)

--- src/main/resources/db/migration/V1_0_0__init_tables.sql
+++ src/main/resources/db/migration/V1_0_0__init_tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS currency (
                        id INTEGER CONSTRAINT currency_pk PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
                        code VARCHAR(3) NOT NULL,
                        full_name VARCHAR(50) NOT NULL,
-                       sign VARCHAR(5) NOT NULL,
+                       sign VARCHAR(1) NOT NULL,
                        unique (code)
 );
 
@@ -10,6 +10,6 @@ CREATE TABLE IF NOT EXISTS exchange_rate (
                                         id INTEGER CONSTRAINT exchange_rate_pk PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
                                         base_currency_id INTEGER NOT NULL,
                                         target_currency_id INTEGER NOT NULL,
-                                        rate DECIMAL(6) NOT NULL,
+                                        rate NUMERIC(10,4) NOT NULL,
                                         unique (base_currency_id,target_currency_id)
 );
